49bca6dbc3d8165d70f017b97c3c76e5a111694a
cbf <malmax5@mail.ru>
2025-05-17 14:15:29 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <QApplication>
+#include <QtWidgets>
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+    QLabel lbl("Hello, World !");
+    lbl.show();
+    return app.exec();
+}
