0d7bf313fed59ab1fa52df37f0d24efdcfa22deb
cbf <malmax5@mail.ru>
2025-05-18 22:18:13 +0300

    Add Pheromone map

---
+++ include/model/PheromoneMap.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <QPointF>
+
+#include <QVector>
+
+#include <cmath>
+#include <algorithm>
+
+class PheromoneMap
+{
+public:
+    explicit PheromoneMap(int width = 100, int height = 100);
+
+    void addPheromone(const QPointF& pos, qreal strength);
+    void evaporate();
+    QPointF getDirection(const QPointF& pos);
+
+private:
+    qreal evaporateRate = 0.95;
+
+    int width;
+    int height;
+    QVector<QVector<qreal>> map;
+
+    inline int toIndexX(qreal x) const;
+    inline int toIndexY(qreal y) const;
+    inline bool isValid(int x, int y) const;
+};
---
+++ src/model/PheromoneMap.cpp
@@ -0,0 +1,89 @@
+#include "../../include/model/PheromoneMap.hpp"
+
+PheromoneMap::PheromoneMap(int width, int height)
+    : width(width), height(height)
+{
+    map.resize(width);
+
+    for (int x = 0; x < width; x++)
+    {
+        map[x].resize(height, 0);
+    }
+}
+
+void PheromoneMap::addPheromone(const QPointF& pos, qreal strength)
+{
+    int x = toIndexX(pos.x());
+    int y = toIndexY(pos.y());
+
+    if (isValid(x, y))
+    {
+        map[x][y] += strength;
+    }
+}
+
+void PheromoneMap::evaporate()
+{
+    for (int x = 0; x < width; x++)
+    {
+        for (int y = 0; y < height; y++)
+        {
+            map[x][y] *= evaporateRate;
+
+            if (map[x][y] - 0.01 < 0)
+            {
+                map[x][y] = 0;
+            }
+        }
+    }
+}
+
+QPointF PheromoneMap::getDirection(const QPointF& pos)
+{
+    int x = toIndexX(pos.x());
+    int y = toIndexY(pos.y());
+
+    if (!isValid(x, y))
+    {
+        return QPointF(0, 0);
+    }
+
+    static const int dx[] = {-1, 0, 1, -1, 1, -1, 0, 1};
+    static const int dy[] ={-1,-1,-1, 0, 0, 1, 1, 1};
+
+    qreal maxStrength = -1;
+    int bestDir = 0;
+
+    for (int i = 0; i < 8; i++)
+    {
+        int nx = dx[i];
+        int ny = dy[i];
+
+        if (isValid(nx, ny))
+        {
+            if (map[nx][ny] > maxStrength)
+            {
+                maxStrength = map[nx][ny];
+                bestDir = i;
+            }
+        }
+    }
+
+    return QPointF(dx[bestDir], dy[bestDir]);
+}
+
+inline int PheromoneMap::toIndexX(qreal x) const
+{
+    return round(x);
+}
+
+inline int PheromoneMap::toIndexY(qreal y) const
+{
+    return round(y);
+}
+
+inline bool PheromoneMap::isValid(int x, int y) const
+{
+    return x >= 0 && y >= 0 && x < width && y < height;
+}
+
